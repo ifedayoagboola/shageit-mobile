3fdfe2ba98611bd36b58d84e5fc0579b44486070
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-08-15 00:44:53 +0100

    crypto tab

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,9 +12,9 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 import * as SecureStore from "expo-secure-store";
 
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { UserInactivityProvider } from "@/context/UserInactivity";
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 // Cache the Clerk JWT
 const tokenCache = {
@@ -204,14 +204,14 @@ const RootLayoutNav = () => {
       publishableKey={CLERK_PUBLISHABLE_KEY!}
       tokenCache={tokenCache}
     >
-      {/* <QueryClientProvider client={queryClient}>
-        <UserInactivityProvider> */}
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <StatusBar style="light" />
-        <InitialLayout />
-      </GestureHandlerRootView>
-      {/* </UserInactivityProvider>
-      </QueryClientProvider> */}
+      <QueryClientProvider client={queryClient}>
+        {/* <UserInactivityProvider> */}
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <StatusBar style="light" />
+          <InitialLayout />
+        </GestureHandlerRootView>
+        {/* </UserInactivityProvider> */}
+      </QueryClientProvider>
     </ClerkProvider>
   );
 };
